70e39904056cbd0f4c9c25882009784d3f80403c
Martina <martina@rivizzigno.it>
2018-05-28 10:37:35 +0200

    remove all style mistakes

--- src/offboard_node.cpp
+++ src/offboard_node.cpp
@@ -56,13 +56,13 @@ int main(int argc, char** argv) {
     if (current_state.mode != "OFFBOARD" &&
         (ros::Time::now() - last_request > ros::Duration(5.0))) {
       if (set_mode_client.call(offb_set_mode) &&
-          offb_set_mode.response.mode_sent){
+          offb_set_mode.response.mode_sent) {
         ROS_INFO("Offboard enabled");
       }
       last_request = ros::Time::now();
     } else {
       if (!current_state.armed &&
-          (ros::Time::now()-last_request > ros::Duration(5.0))) {
+          (ros::Time::now() - last_request > ros::Duration(5.0))) {
         if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
           ROS_INFO("Vehicle armed");
         }
